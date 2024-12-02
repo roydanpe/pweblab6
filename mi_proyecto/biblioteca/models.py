5e476d81716a679249986b3327273fa4d69a28fb
roydanpe <rapazaan@unsa.edu.pe>
2024-12-02 00:11:49 -0500

    aregando los modelos  usuario , libro y review y haciendo las migraciones

--- mi_proyecto/biblioteca/models.py
+++ mi_proyecto/biblioteca/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Modelo para los Usuarios
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+# Modelo para los Libros
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.titulo
+
+# Modelo para las Reviews
+class Review(models.Model):
+    libro = models.ForeignKey(Libro, related_name='reviews', on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, related_name='reviews', on_delete=models.CASCADE)
+    calificacion = models.PositiveIntegerField()  # Calificación entre 1-5
+    comentario = models.TextField()
+
+    def __str__(self):
+        return f"Review de {self.usuario.nombre} para {self.libro.titulo}"
