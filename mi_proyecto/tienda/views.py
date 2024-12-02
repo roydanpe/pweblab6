cd20f1deca89d99758b488a08972b822f7aebd1e
roydanpe <rapazaan@unsa.edu.pe>
2024-12-01 23:41:50 -0500

    creando vistas para manejar el formulario

--- mi_proyecto/tienda/views.py
+++ mi_proyecto/tienda/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .forms import VentaForm
+from .models import Venta
+
+def crear_venta(request):
+    if request.method == 'POST':
+        form = VentaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_ventas')
+    else:
+        form = VentaForm()
+    return render(request, 'tienda/crear_venta.html', {'form': form})
+
+def lista_ventas(request):
+    ventas = Venta.objects.all()
+    return render(request, 'tienda/lista_ventas.html', {'ventas': ventas})
